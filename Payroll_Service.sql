9863977c7b55ac88d5d7dee781d2a39da2a045ef
pavan kumar <raipawan337@gmail.com>
2021-12-16 16:28:18 +0530

    [Pavan]Added: UC2 implemented

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -3,6 +3,16 @@ create database Payroll_Service;
 USE Payroll_Service;
 
 
+/*uc2*/
+create table employee_payroll
+(
+id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+NAME VARCHAR(20) NOT NULL,
+SALARY INT NOT NULL,
+START DATE NOT NULL
+);
+
+
 
 
 
